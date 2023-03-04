0df130ccfd0be75423ad9316bd666b35bde05a6d
Lejla Čarovac-Mehmedović <samir_mehmedovic@live.com>
2023-03-04 15:31:00 +0100

    new style

---
+++ src/components/header/Header.js
@@ -0,0 +1,9 @@
+
+import  './Header.css';
+export const Header  = () =>  {
+    return (
+        <header className='Header'>
+            <h1>My React Blog!</h1>
+        </header>
+    )
+}
---
+++ src/components/main/main.js
@@ -0,0 +1,18 @@
+
+import './main.css'
+
+export const Main = () => {
+    
+    return (
+        <ul className='MainDiv'>
+            <li>
+              
+                <h3>Neki text </h3>
+            
+            </li>
+            <li>
+                <p>Before starting with code decompose UI and decide which components need to be created</p>
+            </li>
+        </ul>
+    )
+}
